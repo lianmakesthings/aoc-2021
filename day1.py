901f0751c9fc98c9d169e5b86c4932698159cfc1
chimney42 <github@chimney42.com>
2021-12-01 22:05:36 +0100

    day1

---
+++ day1.py
@@ -0,0 +1,36 @@
+import sys
+
+def first(filepath):
+  f = open(filepath, "r")
+  lastVal = sys.maxsize
+  increments = 0
+  
+  for line in f.readlines():
+    val = int(line)
+    if (val > lastVal):
+      increments += 1
+
+    lastVal = val
+
+  return increments
+
+def second(filepath):
+  f = open(filepath, "r")
+  lines = f.readlines()
+
+  lastSum = sys.maxsize
+  increments = 0
+
+  for i in range(0, len(lines)-2):
+    val = int(lines[i])
+    val1 = int(lines[i+1])
+    val2 = int(lines[i+2])
+    sum = val + val1 + val2
+    if (sum > lastSum):
+      increments += 1
+
+    lastSum = sum
+
+  return increments
+
+print(second("input1.txt"))
