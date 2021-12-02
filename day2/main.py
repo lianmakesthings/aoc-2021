ffbffe278d8f07d7dc650462dbf8a14d3937d3a4
chimney42 <github@chimney42.com>
2021-12-02 11:15:16 +0100

    day2 part 2

--- day2/main.py
+++ day2/main.py
@@ -22,4 +22,28 @@ def first(filepath):
 
   return position * depth
 
-print(first('input.txt'))
+def second(filepath):
+  f = open(filepath, "r")
+  regex = re.compile('([a-z]+) (\\d)')
+  aim = 0
+  position = 0
+  depth = 0
+
+  for line in f.readlines():
+    matches = regex.findall(line)
+    direction = matches[0][0]
+    val = int(matches[0][1])
+
+    if ('forward' == direction):
+      position += val
+      depth += aim * val
+    elif ('down' == direction):
+      aim += val
+    elif ('up' == direction):
+      aim -= val
+    else:
+      print('something went wrong')
+
+  return position * depth
+
+print(second('input.txt'))
--- day2/test_main.py
+++ day2/test_main.py
@@ -1,4 +1,7 @@
-from main import first
+from main import first, second
 
 def test_first():
-    assert first("test.txt") == 150
+  assert first("test.txt") == 150
+
+def test_second():
+  assert second("test.txt") == 900
