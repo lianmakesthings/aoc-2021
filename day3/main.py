6f09d51796ea0db5e3ca2ba5c9b1689b184cd584
chimney42 <github@chimney42.com>
2021-12-03 18:12:46 +0100

    refactoring

--- day3/main.py
+++ day3/main.py
@@ -2,6 +2,18 @@ def readFile(filepath):
   f = open(filepath, "r")
   return f.readlines()
 
+def mostCommonBit(list):
+  bit = max(set(list), key=list.count)
+  if min(set(list), key=list.count) == bit:
+      bit = "1"
+  return bit
+
+def leastCommonBit(list):
+  bit = min(set(list), key=list.count)
+  if max(set(list), key=list.count) == bit:
+      bit = "0"
+  return bit
+
 
 def first(filepath):
   lines = [line.replace("\n", "") for line in readFile(filepath)]
@@ -10,8 +22,8 @@ def first(filepath):
   
   for i in range(len(lines[0])):
     bitColumn = [x[i] for x in lines]
-    gamma += max(set(bitColumn), key=bitColumn.count)
-    epsilon += min(set(bitColumn), key=bitColumn.count)
+    gamma += mostCommonBit(bitColumn)
+    epsilon += leastCommonBit(bitColumn)
   
   return int(gamma, 2) * int(epsilon, 2)
 
@@ -24,24 +36,16 @@ def second(filepath):
       break
 
     bitColumn = [x[i] for x in lines]
-    commonBit = max(set(bitColumn), key=bitColumn.count)
-    if min(set(bitColumn), key=bitColumn.count) == commonBit:
-      leastCommonBit = "1"
-
-    lines = [x for x in lines if x[i] == commonBit]
+    lines = [x for x in lines if x[i] == mostCommonBit(bitColumn)]
   oxy = int(lines[0], 2)
-  
+
   lines = allLines
   for i in range(len(lines[0])):
     if 1 == len(lines):
       break
 
     bitColumn = [x[i] for x in lines]
-    leastCommonBit = min(set(bitColumn), key=bitColumn.count)
-    if max(set(bitColumn), key=bitColumn.count) == leastCommonBit:
-      leastCommonBit = "0"
-  
-    lines = [x for x in lines if x[i] == leastCommonBit]
+    lines = [x for x in lines if x[i] == leastCommonBit(bitColumn)]
   co2 = int(lines[0], 2)
 
   return oxy * co2
