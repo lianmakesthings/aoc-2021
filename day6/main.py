cd65850ad5bc2cbc71cda14228f3ef097f5228ab
chimney42 <github@chimney42.com>
2021-12-06 18:27:13 +0100

    day 6 part 2

--- day6/main.py
+++ day6/main.py
@@ -2,16 +2,38 @@ def readFile(filepath):
   f = open(filepath, "r")
   return f.readlines()
 
-def first(filepath):
-  days = 80
-  fish = [int(f) for f in readFile(filepath)[0].split(",")]
-
+def loop(fishMap, days):
   while days > 0:
-    newFishCount = sum(1 if f == 0 else 0 for f in fish)
-    fish = [f-1 if f > 0 else 6 for f in fish]
-    fish += [8 for f in range(newFishCount)]
-    days = days-1
+    newFishMap = {}
+    for x in range(0, 9):
+      newFishMap[8] = fishMap[0]
+      print(x, fishMap, newFishMap)
+      if x > 0:
+        newFishMap[x-1] = fishMap[x] + newFishMap[x-1] if x-1 in newFishMap else fishMap[x]
+      else:
+        newFishMap[6] = fishMap[x]
 
-  return len(fish)
+    print(fishMap, newFishMap)
+    fishMap = newFishMap
+    days -= 1
 
-print(first("input.txt"))
+  return newFishMap
+
+def first(filepath):
+  fishList = [int(f) for f in readFile(filepath)[0].split(",")]
+  fishMap = {}
+  for x in range(0, 9):
+    fishMap[x] = sum(1 if f == x else 0 for f in fishList)
+  newFishMap = loop(fishMap, 80)
+
+  return sum(newFishMap.values())
+
+def second(filepath):
+  fishList = [int(f) for f in readFile(filepath)[0].split(",")]
+  fishMap = {}
+  for x in range(0, 9):
+    fishMap[x] = sum(1 if f == x else 0 for f in fishList)
+  newFishMap = loop(fishMap, 256)
+  return sum(newFishMap.values())
+
+print(second("input.txt"))
--- day6/test_main.py
+++ day6/test_main.py
@@ -1,4 +1,7 @@
-from main import first
+from main import first, second
 
 def test_first():
   assert first("test.txt") == 5934
+
+def test_second():
+  assert second("test.txt") == 26984457539
