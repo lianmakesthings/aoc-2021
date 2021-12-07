97da6a72e1e6554ccae7fbe3c23afb26b4992c6d
chimney42 <github@chimney42.com>
2021-12-07 11:38:18 +0100

    day 7

---
+++ day7/main.py
@@ -0,0 +1,23 @@
+import sys
+
+def readFile(filepath):
+  f = open(filepath, "r")
+  return f.readlines()
+
+def first(filepath):
+  input = [int(x) for x in readFile(filepath)[0].split(',')]
+  minFuel = sys.maxsize
+  for p in range(min(input), max(input)):
+    fuel = sum([abs(p-x) for x in input])
+    minFuel = min(minFuel, fuel)
+  return minFuel
+
+def second(filepath):
+  input = [int(x) for x in readFile(filepath)[0].split(',')]
+  minFuel = sys.maxsize
+  for p in range(min(input), max(input)):
+    fuel = sum([(abs(p-x)+1) * (abs(p-x)//2) + ((abs(p-x)%2)*((abs(p-x)+1)/2)) for x in input])
+    minFuel = min(minFuel, fuel)
+  return minFuel
+
+print(second("input.txt"))
