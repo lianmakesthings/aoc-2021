d02163f7558e69e1fb6c0cfe6c84f97bb7433ca4
chimney42 <github@chimney42.com>
2021-12-09 12:29:56 +0100

    day 9 part 2

--- day9/main.py
+++ day9/main.py
@@ -1,3 +1,5 @@
+import math
+
 def readFile(filepath):
   f = open(filepath, "r")
   return f.readlines()
@@ -15,16 +17,18 @@ def buildMap(filepath):
   heightMap.sort(key=sortByVal, reverse=True)
   return heightMap
 
+def getAdjacentPoints(p, map):
+  pointX = p[0]
+  pointY = p[1]
+  return [x for x in map if (p is not x and ((x[0] == pointX and x[1] in range(pointY-1, pointY+2)) or (x[1] == pointY and x[0] in range(pointX-1, pointX+2))))]
+
 def calcLowPoints(heightMap):
   lowPoints = []
   unchecked = heightMap
   while len(unchecked) > 0:
     point = unchecked.pop()
-    pointX = point[0]
-    pointY = point[1]
-    pointValue = point[2]
-    adjacentPoints = [x for x in heightMap if (point is not x and ((x[0] == pointX and x[1] in range(pointY-1, pointY+2)) or (x[1] == pointY and x[0] in range(pointX-1, pointX+2))))]
-    if all(pointValue < p[2] for p in adjacentPoints):
+    adjacentPoints = getAdjacentPoints(point, heightMap)
+    if all(point[2] < p[2] for p in adjacentPoints):
       lowPoints.append(point)
       unchecked = [x for x in unchecked if not x in adjacentPoints]
   return lowPoints
@@ -33,3 +37,22 @@ def first(filepath):
   heightMap = buildMap(filepath)
   lowPoints = calcLowPoints(heightMap)
   return sum([x[2] for x in lowPoints]) + len(lowPoints)
+
+def second(filepath):
+  heightMap = buildMap(filepath)
+  lowPoints = calcLowPoints(heightMap)
+  basinSize = []
+  for p in lowPoints:
+    basin = {p}
+    adjacentPointsNotNine = [x for x in getAdjacentPoints(p, heightMap) if x[2] < 9 and x not in basin]
+    
+    while len(adjacentPointsNotNine) > 0:
+      basin.update(adjacentPointsNotNine)
+      adjacentPointsNotNine = [i for s in [getAdjacentPoints(x, heightMap) for x in adjacentPointsNotNine] for i in s if i[2] < 9 and i not in basin]
+      
+    basinSize.append(len(basin))
+  basinSize.sort(reverse=True)
+  
+  return math.prod(basinSize[:3])
+
+print(second("input.txt"))
