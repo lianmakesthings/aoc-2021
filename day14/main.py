98cc8a6e697874d015262bbfd23ff5fe4aff0fcc
chimney42 <github@chimney42.com>
2021-12-14 22:19:45 +0100

    day 14 part 2

--- day14/main.py
+++ day14/main.py
@@ -1,33 +1,48 @@
+
 def readFile(filepath):
   f = open(filepath, "r")
   return f.readlines()
 
 def first(filepath, steps):
   input = [x.replace("\n", "") for x in readFile(filepath)]
-  polymer = input.pop(0)
+  
+  stringBeginning = input.pop(0)
+  firstChar = stringBeginning[0]
+  lastChar = stringBeginning[-1]
+  polymer = {}
+  for i in range(len(stringBeginning)):
+    if i < len(stringBeginning)-1:
+      pair = stringBeginning[i:i+2]
+      count = polymer[pair] +1 if pair in polymer else 1
+      polymer[pair] = count
   input.pop(0)
+
   insertionRules = {}
   for l in input:
     key, val = l.split(" -> ")
-    insertionRules[key] = val
-
+    insertionRules[key] = [key[0]+val, val+key[1]]
   while steps > 0:
-    print(steps)
-    toReplace = {}
-    for key in insertionRules.keys():
-      occurences = [i for i in range(len(polymer)) if polymer.startswith(key, i)]
-      for i in occurences:
-        toReplace[i+1] = insertionRules[key]
+    pairs = [x for x in polymer.keys() if polymer[x] > 0]
+    newPolymer = {}
     
-    toReplaceIndices = list(toReplace.keys())
-    toReplaceIndices.sort()
-    print(len(toReplaceIndices))
-    for i in toReplaceIndices:
-      polymer = polymer[:i+toReplaceIndices.index(i)] + toReplace[i] + polymer[i+toReplaceIndices.index(i):]
-
+    for p in pairs:
+      for np in insertionRules[p]:
+        count = newPolymer[np]+polymer[p] if np in newPolymer else polymer[p]
+        newPolymer[np] = count
+      
+    polymer = newPolymer
     steps -= 1
-  uniqueLetters = set(polymer)
-  counts = [polymer.count(x) for x in uniqueLetters]
-  return max(counts) - min(counts)
+
+  letterCount = {}
+  for pair in polymer.keys():
+    for c in pair:
+      count = letterCount[c]+polymer[pair] if c in letterCount else polymer[pair]
+      letterCount[c] = count
+
+  letterCount[firstChar] = letterCount[firstChar]+1
+  letterCount[lastChar] = letterCount[lastChar]+1
+
+  letterCount = [x/2 for x in letterCount.values()]
+  return max(letterCount) - min(letterCount)
 
 print(first("input.txt", 40))
