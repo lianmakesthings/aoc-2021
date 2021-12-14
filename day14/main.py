719c4ad951a192cf7af706a8b978facba53beebb
chimney42 <github@chimney42.com>
2021-12-14 14:40:41 +0100

    optimised

--- day14/main.py
+++ day14/main.py
@@ -2,7 +2,7 @@ def readFile(filepath):
   f = open(filepath, "r")
   return f.readlines()
 
-def first(filepath):
+def first(filepath, steps):
   input = [x.replace("\n", "") for x in readFile(filepath)]
   polymer = input.pop(0)
   input.pop(0)
@@ -11,22 +11,23 @@ def first(filepath):
     key, val = l.split(" -> ")
     insertionRules[key] = val
 
-  steps = 10
   while steps > 0:
-    newPolymer = ""
-    for c in range(len(polymer)):
-      pair = polymer[c:c+2]
-      if len(pair) < 2:
-        continue
-      threesome = pair[0] + insertionRules[pair] + pair[1]
-      if len(newPolymer) < 1:
-        newPolymer = threesome
-      else:
-        newPolymer += threesome[1:3]
-    polymer = newPolymer
+    print(steps)
+    toReplace = {}
+    for key in insertionRules.keys():
+      occurences = [i for i in range(len(polymer)) if polymer.startswith(key, i)]
+      for i in occurences:
+        toReplace[i+1] = insertionRules[key]
+    
+    toReplaceIndices = list(toReplace.keys())
+    toReplaceIndices.sort()
+    print(len(toReplaceIndices))
+    for i in toReplaceIndices:
+      polymer = polymer[:i+toReplaceIndices.index(i)] + toReplace[i] + polymer[i+toReplaceIndices.index(i):]
+
     steps -= 1
   uniqueLetters = set(polymer)
-  counts = [polymer.count(x) for x in list(uniqueLetters)]
+  counts = [polymer.count(x) for x in uniqueLetters]
   return max(counts) - min(counts)
 
-print(first("input.txt"))
+print(first("input.txt", 40))
--- day14/test_main.py
+++ day14/test_main.py
@@ -1,4 +1,7 @@
 from main import first
 
 def test_first():
-  assert first("test.txt") == 1588
+  assert first("test.txt", 10) == 1588
+
+def test_second():
+  assert first("test.txt", 40) == 2188189693529
