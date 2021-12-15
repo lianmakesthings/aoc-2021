574d395a6d58ef4a7e7b5927b940ed14cf472a86
chimney42 <github@chimney42.com>
2021-12-15 12:09:20 +0100

    refactor

--- day15/main.py
+++ day15/main.py
@@ -15,18 +15,9 @@ def hasPoint(map, coord):
   points = [x for x in map if x[0] == coord[0] and x[1] == coord[1]]
   return map.index(points[0]) if len(points) > 0 else False
 
-def first(filepath):
-  input = readFile(filepath)
-  map = []
-  maxX = 0
-  maxY = 0
-  for y in range(len(input)):
-    line = input[y]
-    maxY = max(maxY, y)
-    for x in range(len(input[0])):
-      map.append((x, y, int(line[x])))
-      maxX = max(maxX, x)
-  map.reverse()
+def buildRiskMap(map):
+  maxX = max([x[0] for x in map])
+  maxY = max([x[1] for x in map])
   riskmap = [(maxX, maxY, 0)]
   for nextPoint in map:
     print(map.index(nextPoint), len(map))
@@ -38,8 +29,19 @@ def first(filepath):
         riskmap.append(p)
       elif not hasPoint(riskmap, p):
         riskmap.append(p)
+  return riskmap
+
+def first(filepath):
+  input = readFile(filepath)
+  map = []
+  for y in range(len(input)):
+    line = input[y]
+    for x in range(len(input[0])):
+      map.append((x, y, int(line[x])))
+  map.reverse()
+  riskmap = buildRiskMap(map)
   return [x[2] for x in riskmap if x[0] == 0 and x[1] == 0][0]
 
-print(first("input.txt"))
+#print(first("input.txt"))
 
   
